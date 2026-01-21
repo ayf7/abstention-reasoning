85deabc049fa4af5b5349218f4def1f2a4b71670
ayf7 <anthonyf1223@gmail.com>
2026-01-21 10:58:34 -0500

    delete original data

--- data_new/countdown/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Countdown task module."""
-from data_new.countdown.generator import CountdownPuzzleGenerator
-from data_new.countdown.manager import CountdownManager
-
-__all__ = ["CountdownManager", "CountdownPuzzleGenerator"]
